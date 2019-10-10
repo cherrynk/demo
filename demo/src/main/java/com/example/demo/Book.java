3453c1b6e25e6fe451eadc23109bf3f3e5e780ae
merlini <nk_gg@163.com>
2019-10-10 20:26:15 +0800

    1

---
+++ demo/src/main/java/com/example/demo/Book.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class Book {
+	@Value("${book.name}")
+	private String name;
+	@Value("${book.author}")
+	private String author;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+}
